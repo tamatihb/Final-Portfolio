7c7525c74c0ac37279c22121558422f1c4f7ef87
Tamati <78838324+tamatihb@users.noreply.github.com>
2022-04-19 21:41:26 +1000

    Add files via upload
    Final Portfolio

---
+++ build/precache-manifest.880da093b0cc95f39646b1e906fd811c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "357290fcc37785b811060351dc626fd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e25461a5b66dea463ef",
+    "url": "/static/css/main.10390fc8.chunk.css"
+  },
+  {
+    "revision": "9cb1e171d554412911a8",
+    "url": "/static/js/2.16cbb229.chunk.js"
+  },
+  {
+    "revision": "1bde7cbddc37293b60694441b834365b",
+    "url": "/static/js/2.16cbb229.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e25461a5b66dea463ef",
+    "url": "/static/js/main.9a2d5ef8.chunk.js"
+  },
+  {
+    "revision": "f8f21f79e186842fe33cd40c4ad46823",
+    "url": "/static/js/main.9a2d5ef8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc454f581cdc072df54e",
+    "url": "/static/js/runtime-main.4c381e45.js"
+  },
+  {
+    "revision": "f401fb1f1f7e4ec42d9122c536692c11",
+    "url": "/static/media/ChefsHat.f401fb1f.png"
+  },
+  {
+    "revision": "eb64d1aaecf43d9dc20d5b1777c06df0",
+    "url": "/static/media/Integrated.eb64d1aa.svg"
+  },
+  {
+    "revision": "7f0c97bf932a33ef6503c0cf5fbbd786",
+    "url": "/static/media/Next.7f0c97bf.png"
+  },
+  {
+    "revision": "0383e861303804de047ab614921a78e4",
+    "url": "/static/media/Succes.0383e861.png"
+  },
+  {
+    "revision": "9b1e02cfeceb771cbae3cfd113a927a5",
+    "url": "/static/media/video.9b1e02cf.mp4"
+  },
+  {
+    "revision": "2c869bccd630c6e5dbd6df957a01cb9a",
+    "url": "/static/media/website1.2c869bcc.png"
+  }
+]);
